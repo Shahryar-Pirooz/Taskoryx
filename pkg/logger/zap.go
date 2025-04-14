e62d8b339493e89b00b1be7f21e8a57f35157bbf
shahryar <dev.shahryar@gmail.com>
2025-04-14 21:24:30 +0330

    feat : add logger

---
+++ pkg/logger/zap.go
@@ -0,0 +1,36 @@
+package logger
+
+import (
+	"sync"
+
+	"go.uber.org/zap"
+)
+
+var (
+	logger *zap.Logger
+	once   sync.Once
+)
+
+func Init(isProduction bool) {
+	once.Do(func() {
+		var cfg zap.Config
+
+		if isProduction {
+			cfg = zap.NewProductionConfig()
+		} else {
+			cfg = zap.NewDevelopmentConfig()
+		}
+
+		var err error
+
+		logger, err = cfg.Build()
+		if err != nil {
+			panic("Failed to initialize logger: " + err.Error())
+		}
+		zap.ReplaceGlobals(logger)
+	})
+}
+
+func GetLogger() *zap.Logger {
+	return logger
+}
