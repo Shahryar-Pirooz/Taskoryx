d81423b8ea606d20465a1d65094d50ea4878f1b4
shahryar <dev.shahryar@gmail.com>
2025-04-23 12:58:59 +0330

    fix: fix empty items in get users

--- pkg/fp/mappers.go
+++ pkg/fp/mappers.go
@@ -1,7 +1,7 @@
 package fp
 
 func Map[T, U any](s []T, mapFunction func(T) *U) []U {
-	result := make([]U, len(s))
+	result := make([]U, 0, len(s))
 	for _, ptr := range s {
 		result = append(result, *mapFunction(ptr))
 	}
