93e85e10691d8db6dcf3a1122ff2806b7aa9075e
shahryar <dev.shahryar@gmail.com>
2025-05-05 18:29:10 +0330

    refactor: streamline token generation by consolidating access and refresh token logic

--- pkg/jwt/auth.go
+++ pkg/jwt/auth.go
@@ -20,30 +20,40 @@ func NewJWTManager(secretKey []byte, accessTTL, refreshTTL time.Duration) *JWTMa
 	}
 }
 
-func (jm *JWTManager) GenerateToken(userID string) (string, string, error) {
+func (jm *JWTManager) generateToken(userID string, ttl time.Duration, now time.Time) (string, error) {
+	claim := CustomClaims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(now),
+			ExpiresAt: jwt.NewNumericDate(now.Add(ttl)),
+		},
+	}
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claim).SignedString(jm.secretKey)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+func (jm *JWTManager) GenerateAccessToken(userID string) (string, error) {
 	now := time.Now()
-	accessClaim := CustomClaims{
-		UserID: userID,
-		RegisteredClaims: jwt.RegisteredClaims{
-			IssuedAt:  jwt.NewNumericDate(now),
-			ExpiresAt: jwt.NewNumericDate(now.Add(jm.accessTTL)),
-		},
-	}
-	refreshClaim := CustomClaims{
-		UserID: userID,
-		RegisteredClaims: jwt.RegisteredClaims{
-			IssuedAt:  jwt.NewNumericDate(now),
-			ExpiresAt: jwt.NewNumericDate(now.Add(jm.refreshTTL)),
-		},
-	}
-	accessToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, accessClaim).SignedString(jm.secretKey)
+	return jm.generateToken(userID, jm.accessTTL, now)
+}
+func (jm *JWTManager) GenerateRefreshToken(userID string) (string, error) {
+	now := time.Now()
+	return jm.generateToken(userID, jm.refreshTTL, now)
+}
+
+func (jm *JWTManager) GeneratePairToken(userID string) (string, string, error) {
+	accessToken, err := jm.GenerateAccessToken(userID)
 	if err != nil {
 		return "", "", err
 	}
-	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaim).SignedString(jm.secretKey)
+	refreshToken, err := jm.GenerateRefreshToken(userID)
 	if err != nil {
-		return "", "", nil
+		return "", "", err
 	}
+
 	return accessToken, refreshToken, nil
 }
 
@@ -52,7 +62,7 @@ func (jm *JWTManager) ValidationToken(tokenString string) (*CustomClaims, error)
 		return jm.secretKey, nil
 	})
 	if err != nil {
-		return nil, nil
+		return nil, err
 	}
 	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
 		return claims, nil
