b7742f8f7605cf6dacd9f997064e785755036cc5
shahryar <dev.shahryar@gmail.com>
2025-05-06 14:07:21 +0330

    refactor: simplify JWT token generation and validation by removing JWTManager struct

--- pkg/jwt/auth.go
+++ pkg/jwt/auth.go
@@ -6,21 +6,8 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-type JWTManager struct {
-	secretKey  []byte
-	accessTTL  time.Duration
-	refreshTTL time.Duration
-}
-
-func NewJWTManager(secretKey []byte, accessTTL, refreshTTL time.Duration) *JWTManager {
-	return &JWTManager{
-		secretKey:  secretKey,
-		accessTTL:  accessTTL,
-		refreshTTL: refreshTTL,
-	}
-}
-
-func (jm *JWTManager) generateToken(userID string, ttl time.Duration, now time.Time) (string, error) {
+func GenerateToken(userID, secretKey string, ttl time.Duration) (string, error) {
+	now := time.Now()
 	claim := CustomClaims{
 		UserID: userID,
 		RegisteredClaims: jwt.RegisteredClaims{
@@ -28,38 +15,16 @@ func (jm *JWTManager) generateToken(userID string, ttl time.Duration, now time.T
 			ExpiresAt: jwt.NewNumericDate(now.Add(ttl)),
 		},
 	}
-	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claim).SignedString(jm.secretKey)
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claim).SignedString(secretKey)
 	if err != nil {
 		return "", err
 	}
 	return token, nil
 }
 
-func (jm *JWTManager) GenerateAccessToken(userID string) (string, error) {
-	now := time.Now()
-	return jm.generateToken(userID, jm.accessTTL, now)
-}
-func (jm *JWTManager) GenerateRefreshToken(userID string) (string, error) {
-	now := time.Now()
-	return jm.generateToken(userID, jm.refreshTTL, now)
-}
-
-func (jm *JWTManager) GeneratePairToken(userID string) (string, string, error) {
-	accessToken, err := jm.GenerateAccessToken(userID)
-	if err != nil {
-		return "", "", err
-	}
-	refreshToken, err := jm.GenerateRefreshToken(userID)
-	if err != nil {
-		return "", "", err
-	}
-
-	return accessToken, refreshToken, nil
-}
-
-func (jm *JWTManager) ValidationToken(tokenString string) (*CustomClaims, error) {
+func ValidationToken(tokenString, secretKey string) (*CustomClaims, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(t *jwt.Token) (interface{}, error) {
-		return jm.secretKey, nil
+		return secretKey, nil
 	})
 	if err != nil {
 		return nil, err
