3adb489d8cddc3ac1845972de8bbcebdda2a6c75
Shahryar Pi <dev.shahryar@gmail.com>
2025-05-21 21:52:32 +0330

    feat: add Redis configuration to config files

--- config/config.go
+++ config/config.go
@@ -3,8 +3,9 @@ package config
 type Config struct {
 	Server     ServerConfig `mapstructure:"http"`
 	Database   Database     `mapstructure:"database"`
-	Production bool         `mapstructure:"production"`
+	Redis      Redis        `mapstructure:"redis"`
 	Jwt        JWT          `mapstructure:"jwt"`
+	Production bool         `mapstructure:"production"`
 }
 
 type ServerConfig struct {
@@ -18,6 +19,18 @@ type Database struct {
 	Password string `mapstructure:"password"`
 	Port     string `mapstructure:"port"`
 	User     string `mapstructure:"user"`
+}
+
+type Redis struct {
+	Host         string `mapstructure:"host"`
+	Port         string `mapstructure:"port"`
+	Password     string `mapstructure:"password"`
+	DB           string `mapstructure:"db"`
+	PoolSize     string `mapstructure:"poolsize"`
+	DialTimeout  string `mapstructure:"dialtimeout"`
+	ReadTimeout  string `mapstructure:"readtimeout"`
+	WriteTimeout string `mapstructure:"writetimeout"`
+	IdleTimeout  string `mapstructure:"idletimeout"`
 }
 
 type JWT struct {
